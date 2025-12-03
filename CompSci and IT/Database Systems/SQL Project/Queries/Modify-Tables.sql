a76f29fd77d13fb61ef64f142a2d968cd4a977a9
Claudé Davis <claudedavis2024@gmail.com>
2025-12-03 10:46:51 -0500

    file to note modifications made to tables

---
+++ CompSci and IT/Database Systems/SQL Project/Queries/Modify-Tables.sql
@@ -0,0 +1,17 @@
+
+--@block
+ALTER TABLE Service
+MODIFY COLUMN service_type VARCHAR(100) NOT NULL;
+
+--@block
+ALTER TABLE Product
+ADD FOREIGN KEY (id) REFERENCES Offering(id)
+;
+
+ALTER TABLE Service
+ADD FOREIGN KEY (id) REFERENCES Offering(id)
+;
+
+--@block
+ALTER TABLE Offering
+ADD offering_type VARCHAR(50) NOT NULL;
