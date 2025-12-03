f9b48978d7e428b0ae680ae3db97751b83cf7797
Claudé Davis <claudedavis2024@gmail.com>
2025-12-03 09:22:50 -0500

    sql file for inserting values into db tables

---
+++ CompSci and IT/Database Systems/SQL Project/Queries/Insert-Values.sql
@@ -0,0 +1,12 @@
+INSERT INTO Customer(name, address, phone, email, birthdate, age)
+VALUES ('Hanson Hodge', 'Atlanta, GA', 4705456789, 'hHodge@companyname.com', 1976-08-15, 49),
+('Joanna Perl', 'Alpharetta, GA', 4047349991, 'itzJo@gmail.com', 1992-07-23, 33),
+('Areli Castro', 'Sacramento, CA', 3419095522, 'areliC98@gmail.com', 1998-11-03, 27),
+('Chase Dereks', 'Dallas, TX', 8066752342, 'chase94@outlook.com', 1994-05-30, 31),
+('Julie Evans', 'Richmond, VA', 6863412390, 'julieE@companyname2.com', 1981-12-17, 43)
+;
+
+INSERT INTO Customer(name, address, email, birthdate, age)
+VALUES ('John Russo', 'Brooklyn, NY', 'russoJ@gmail.com', 1998-05-27, 27),
+('Sandy Martinez', 'Jonesboro, GA', 'sandyM@companyname.net', 2000-04-04, 25),
+;
