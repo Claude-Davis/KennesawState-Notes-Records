cb84a48d38e619b6da53029328d77693480a46ff
Claudé Davis <claudedavis2024@gmail.com>
2025-12-03 17:23:30 -0500

    formatting updated

--- CompSci and IT/Database Systems/SQL Project/Queries/Updated Version/updated p2 schema.sql
+++ CompSci and IT/Database Systems/SQL Project/Queries/Updated Version/updated p2 schema.sql
@@ -1,5 +1,6 @@
+
 CREATE TABLE `Customer`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `id` INT NOT NULL PRIMARY KEY,
     `name` VARCHAR(50) NOT NULL,
     `address` VARCHAR(50) NOT NULL,
     `phone` BIGINT NULL,
@@ -7,58 +8,59 @@ CREATE TABLE `Customer`(
     `birthdate` DATE NOT NULL,
     `age` BIGINT NULL
 );
-CREATE TABLE `Billing`(
-    `composite_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `service_id` BIGINT NOT NULL,
-    `service_date` DATE NOT NULL COMMENT 'FK of Service(service_date)',
-    `due_date` DATE NOT NULL,
-    `amount` DECIMAL(6, 2) NOT NULL,
-    `client_id` INT NULL,
-    PRIMARY KEY(`service_id`)
+
+CREATE TABLE `Offering`(
+    `id` INT NOT NULL PRIMARY KEY,
+    `description` VARCHAR(100) NOT NULL 
 );
-CREATE TABLE `Service`(
-    `offering_id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `company_unit` VARCHAR(50) NOT NULL,
-    `service_condition` BIGINT NOT NULL
-);
-CREATE TABLE `Purchase`(
-    `offering_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `customer_id` INT NOT NULL,
-    `date_purchased` DATE NOT NULL,
-    `contact_person` VARCHAR(50) NOT NULL,
-    PRIMARY KEY(`customer_id`)
-);
+--description as subtype discriminator
+
 CREATE TABLE `Product`(
-    `offering_id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `offering_id` INT NOT NULL PRIMARY KEY,
+    FOREIGN KEY (offering_id) REFERENCES `Offering`(id),
     `name` VARCHAR(50) NOT NULL,
     `first_release_date` DATE NOT NULL,
     `standard_price` DECIMAL(8, 2) NOT NULL
 );
-CREATE TABLE `Offering`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `description` VARCHAR(100) NOT NULL
+
+CREATE TABLE `Service`(
+    `offering_id` INT NOT NULL PRIMARY KEY,
+    FOREIGN KEY (offering_id) REFERENCES `Offering`(id),
+    `company_unit` VARCHAR(50) NOT NULL,
+    `service_condition` VARCHAR(100) NOT NULL  
 );
+
+
 CREATE TABLE `Repair`(
-    `service_id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `service_id` INT NOT NULL PRIMARY KEY,
+    FOREIGN KEY (service_id) REFERENCES `Service`(offering_id),
     `cost` DECIMAL(8, 2) NOT NULL
 );
 CREATE TABLE `Maintenance`(
-    `service_id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `service_id` INT NOT NULL PRIMARY KEY,
+    FOREIGN KEY (service_id) REFERENCES `Service`(offering_id),
     `hourly_rate` DECIMAL(4, 2) NOT NULL
 );
-ALTER TABLE
-    `Service` ADD CONSTRAINT `service_offering_id_foreign` FOREIGN KEY(`offering_id`) REFERENCES `Repair`(`service_id`);
-ALTER TABLE
-    `Offering` ADD CONSTRAINT `offering_id_foreign` FOREIGN KEY(`id`) REFERENCES `Product`(`offering_id`);
-ALTER TABLE
-    `Customer` ADD CONSTRAINT `customer_id_foreign` FOREIGN KEY(`id`) REFERENCES `Billing`(`composite_id`);
-ALTER TABLE
-    `Service` ADD CONSTRAINT `service_offering_id_foreign` FOREIGN KEY(`offering_id`) REFERENCES `Billing`(`composite_id`);
-ALTER TABLE
-    `Service` ADD CONSTRAINT `service_offering_id_foreign` FOREIGN KEY(`offering_id`) REFERENCES `Maintenance`(`service_id`);
-ALTER TABLE
-    `Purchase` ADD CONSTRAINT `purchase_offering_id_foreign` FOREIGN KEY(`offering_id`) REFERENCES `Customer`(`id`);
-ALTER TABLE
-    `Purchase` ADD CONSTRAINT `purchase_offering_id_foreign` FOREIGN KEY(`offering_id`) REFERENCES `Offering`(`id`);
-ALTER TABLE
-    `Offering` ADD CONSTRAINT `offering_id_foreign` FOREIGN KEY(`id`) REFERENCES `Service`(`offering_id`);
+
+CREATE TABLE `Purchase`(
+    `offering_id` INT NOT NULL,
+    FOREIGN KEY (offering_id) REFERENCES `Offering`(id),
+    `customer_id` INT NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES `Customer`(id),
+    PRIMARY KEY (customer_id, offering_id),
+    `date_purchased` DATE NOT NULL,
+    `contact_person` VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE `Bill`(
+    `service_id` INT NOT NULL,
+    FOREIGN KEY (service_id) REFERENCES `Service`(offering_id),
+    `customer_id` INT NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES `Customer`(id),
+    PRIMARY KEY(service_id, customer_id),
+    `service_date` DATE NOT NULL,
+    `amount_due` DECIMAL(8,2) NOT NULL,
+    `date_due` DATE NOT NULL,
+    `client_id` INT NULL
+);
+
