96bb6eb9557a3040cc25c17da3790e36239733a2
Claudé Davis <claudedavis2024@gmail.com>
2025-12-05 09:22:36 -0500

    insert values

--- CompSci and IT/Database Systems/SQL Project/Class Submissions/SQL file.sql
+++ CompSci and IT/Database Systems/SQL Project/Class Submissions/SQL file.sql
@@ -70,3 +70,31 @@ CREATE TABLE `Bill`(
     `client_id` INT NULL
 );
 
+--@block
+INSERT INTO customer (id, name, address, phone, email, birthdate, age)
+VALUES (1, 'Hanson Jekyll', 'Atlanta, GA', 4705456789, 'hHodge@companyname.com','1976-08-15', 49),
+(2, 'Joanna Perl', 'Alpharetta, GA', 4047349991, 'jPerl@company-name.com', '1992-07-23', 33),
+(3, 'Areli Castro', 'Sacramento, CA', 3419095522, 'areliC@company-name2.com', '1998-11-03', 27),
+(4, 'Chase Dereks', 'Dallas, TX', 8066752342, 'chase6@companyname.net', '1994-05-30', 31),
+(5, 'Julie Evans', 'Richmond, VA', 6863412390, 'julieE@companyname2.com', '1981-12-17', 43),
+(6, 'John Russo', 'Brooklyn, NY', NULL, 'russoJ@co-name.com', '1998-05-27', 27),
+(7, 'Sandy Martinez', 'Jonesboro, GA', NULL, 'sandyM@companyname2.net','2000-04-04', 25)
+;
+
+INSERT INTO offering(id, description)
+VALUES (1, 'Product'),
+(2, 'Product'),
+(3, 'Service'),
+(4, 'Product'),
+(5, 'Service'),
+(6, 'Service'),
+(7, 'Service'),
+(8, 'Product')
+;
+
+INSERT INTO product(offering_id, name, first_release_date, standard_price)
+VALUES (1, 'Laptop', '2000-02-02', 2599.00),
+(2, 'Desktop Computer', '1990-07-07', 850.00),
+(4, 'Solid State Drive', '2016-09-09', 90.00),
+(8, 'Wi-Fi Router', '2003-04-04', 45.99)
+;
