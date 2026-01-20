127c109103c2fc10cb8ab3bfbf8d4e0e0d66d076
Claude-Davis <claudedavis2024@gmail.com>
2026-01-19 19:13:53 -0500

    null value now taken to end number insertion section AFTER valid input has already been given

--- CompSci and IT/Software Engineering/Module 1/AvgCalculator/src/Main.java
+++ CompSci and IT/Software Engineering/Module 1/AvgCalculator/src/Main.java
@@ -8,18 +8,19 @@ public class Main {
         ArrayList<Double> user_inputs = new ArrayList<>();
         
         Boolean run = true;
+        String previous_line_tracker = "";
 
 
         //request user input
         System.out.println("Input a series of numbers to find their average.");
-        System.out.println("After you input a number, press the 'Enter' key to insert the next number.\nInsert a '0' when done.");
+        System.out.println("After you input a number, press the 'Enter' key to insert the next number.\nInsert a '0'  or 'null' when done.");
 
-        //take in user input
+
 
         while (run) {
             String program_counter = sc.nextLine();
-            
             Double verified_num = 0.0;
+
             
             try {
                 verified_num = Double.parseDouble(program_counter);
@@ -28,12 +29,19 @@ public class Main {
                     run = false;
                 } else {
                     user_inputs.add(verified_num);          //add verified input to arraylist
+                    previous_line_tracker = program_counter;
                     //System.out.println(user_inputs);                                                //line to verify input is being saved to arraylist
+                    
                 }
             } catch (NumberFormatException e) {
                 if (program_counter == "") {
-                    System.out.println("Cannot divide by zero. Provide input.");
-                    System.exit(0);
+                    //System.out.println(previous_line_tracker + "PLT");                              //line to check current value of previous_line tracker
+                    if (previous_line_tracker != "") {
+                        run = false;
+                    } else {
+                        System.out.println("Cannot divide by zero. Provide input.");
+                        System.exit(0);
+                    }
                 } else {
                     System.out.println("Your input is not a number.");
                     System.exit(0);
